56c0e9f1ffb8148ab7105898fca376ae2e51e8ec
Mikeshow89 <126121318+Mikeshow89@users.noreply.github.com>
2023-03-24 15:04:00 -0400

    Update PS4.py

--- PS4.py
+++ PS4.py
@@ -1,5 +1,4 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> import pandas as pd
 ... import matplotlib.pyplot as plt
 ... 
